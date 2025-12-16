9ae6c75e6e0cf3ce1ecab77db24bbf613013b370
KkManagalian <Dumbassyahoo2@gmail.com>
2025-12-16 10:29:48 +0200

    Stradajos laiks

--- src/PatstavigaisDarbs.java
+++ src/PatstavigaisDarbs.java
@@ -1,3 +1,5 @@
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Random;
 import java.util.Stack;
 
@@ -8,35 +10,46 @@ public class PatstavigaisDarbs {
 	public static void main(String[] args) {
 		
 		String izvele;
-		int Laiks=0;
 		Random rand = new Random();
 		Stack<Integer> Izlozetie = new Stack<>();
 		Stack<Integer> Laimejosie = new Stack<>();
-
-		
+		long Pauze =60000;
 		String[] darbibas = {"Veikt izlozi", "Laimīgo skaitļu apskate", "Apturēt"};
 		
 		do {
-			
 			izvele=(String)JOptionPane.showInputDialog(null,"Izvēlies darbību", "Darbību saraksts", JOptionPane.PLAIN_MESSAGE, null, darbibas, darbibas[0]);
 			
 			if(izvele==null)
 				break;
 			
+			long Laiks=System.currentTimeMillis();
+			
 			switch(izvele) {
 			case"Veikt izlozi":
-				if(Laiks==0) {
+				if(Laiks>Pauze) {
 					do {
+						while(Laimejosie.size()<3) {
 						for(int i=0;i<10;i++) {
 						int Skaitlis = rand.nextInt(10);
 						Izlozetie.push(Skaitlis);
-						
-						
-						
-						Laimejosie.push(Izlozetie.pop());
-						Laiks+=1;
 						}
+						
+						System.out.println(Izlozetie);
+						for(int i=0;i<rand.nextInt(8)+1;i++) {
+							Izlozetie.pop();
+							}
+						System.out.println(Izlozetie);
+					Laimejosie.push(Izlozetie.pop());
+						}
+						
+						Laiks+=60;
 					}while(Laiks==0);
+					
+					LocalTime IstsLaiks = LocalTime.now();
+					DateTimeFormatter Formats = DateTimeFormatter.ofPattern("HH:mm:ss");
+				    String Formatets = IstsLaiks.format(Formats);
+					JOptionPane.showMessageDialog(null, "Izlozes laiks ir: "+Formatets);
+					Pauze=Laiks+60000;
 
 				}else {
 					JOptionPane.showMessageDialog(null, "Nav pagājusi minūte!","Brīdinājums",JOptionPane.WARNING_MESSAGE);
@@ -59,7 +72,6 @@ public class PatstavigaisDarbs {
 			}
 			
 		}while(!izvele.equals("Apturēt"));
-		
 	}
 
 }
